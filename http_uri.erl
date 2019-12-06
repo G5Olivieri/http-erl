cbb511f7d6a248b15c15077e0b0b7c10fbdd5d27
Glayson Olivieri <glayson@vizir.com.br>
2019-12-06 16:01:12 -0300

    Fix bitstring module call

--- http_uri.erl
+++ http_uri.erl
@@ -4,7 +4,7 @@
 parse_host(Url) when is_list(Url) ->
   parse_host(list_to_binary(Url));
 parse_host(<<"http://", Url/bitstring>>) ->
-  case member(<<"@">>, Url) of
+  case bitstring:member(<<"@">>, Url) of
     true ->
       [_,Host] = string:split(Url, <<"@">>),
       parse_host_1(Host);
@@ -13,7 +13,7 @@ parse_host(<<"http://", Url/bitstring>>) ->
   end.
 
 parse_host_1(Url) ->
-    [Host|_] = tokens(Url, <<":?/">>),
+    [Host|_] = bitstring:tokens(Url, <<":?/">>),
     {host, Host}.
 
 parse_scheme(_) ->
